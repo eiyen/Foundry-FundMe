6dcf2a5b6c3adcce2a138bf88e38912bd3cc31f1
eiyen <937915771@qq.com>
2023-08-11 15:05:34 +0800

    成功将合约部署至测试网，并通过脚本完成捐款和提款的交互

--- script/Interactions.s.sol
+++ script/Interactions.s.sol
@@ -3,16 +3,13 @@ pragma solidity ^0.8.19;
 
 import {Script, console} from "forge-std/Script.sol";
 import {FundMe} from "../src/FundMe.sol";
-import {DevOpsTools} from "../lib/foundry-devops/src/DevOpsTools.sol";
+// import {DevOpsTools} from "../lib/foundry-devops/src/DevOpsTools.sol";
 
 contract FundFundMe is Script {
     uint256 public constant TRADING_FUNDS = 0.01 ether;
 
     function run() public {
-        address recentlyDeployedContractAddress = DevOpsTools.get_most_recent_deployment(
-            "FundMe", 
-            block.chainid
-        );
+        address recentlyDeployedContractAddress = 0xF4276bFB3a2e711aE8E74754d6799E770BBAE211;
         fundFundMe(FundMe(recentlyDeployedContractAddress));
     }
 
@@ -27,10 +24,7 @@ contract WithdrawFundMe is Script {
     uint256 public constant TRADING_FUNDS = 0.01 ether;
 
     function run() public {
-        address recentlyDeployedContractAddress = DevOpsTools.get_most_recent_deployment(
-            "FundMe",
-            block.chainid
-        );
+        address recentlyDeployedContractAddress = 0xF4276bFB3a2e711aE8E74754d6799E770BBAE211;
         withdrawFundMe(FundMe(recentlyDeployedContractAddress));
     }
 
